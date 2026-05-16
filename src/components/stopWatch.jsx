ea395b806cfdf628d2c3adaf351b8def06222f5a
jowcode <jooow346@gamil.com>
2026-05-16 13:39:48 +0300

    add a svg photo

--- src/components/stopWatch.jsx
+++ src/components/stopWatch.jsx
@@ -1,10 +1,13 @@
-
+import './../styles/stopWatch.css';
+import {background} from './../assets/img.js';
 function StopWatch() {
     return (
  
         <div  div className="text-4xl">
             <h2>Stop Watch</h2>
+            {background}
         </div>
+        
     );
 }
 export default StopWatch
